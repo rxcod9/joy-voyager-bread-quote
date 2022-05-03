2b2d8a96f8214c9bfb877682b5567525fb7f8b93
Ramakant <gangwar.ramakant@gmail.com>
2022-05-03 19:24:06 +0530

    💚_Seeder-count

--- database/seeders/DummyQuotesTableSeeder.php
+++ database/seeders/DummyQuotesTableSeeder.php
@@ -14,7 +14,7 @@ class DummyQuotesTableSeeder extends Seeder
      */
     public function run()
     {
-        $count = 100;
+        $count = 20;
         Quote::factory()
             ->count($count)
             ->state(function (array $attributes) use ($count) {
--- database/seeders/QuotesTableSeeder.php
+++ database/seeders/QuotesTableSeeder.php
@@ -18,7 +18,7 @@ class QuotesTableSeeder extends Seeder
             return false;
         }
 
-        $count = 100;
+        $count = 20;
         Quote::factory()
             ->count($count)
             ->state(function (array $attributes) use ($count) {
